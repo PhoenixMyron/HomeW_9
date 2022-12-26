d7bb6732b566f0a28ed27fc4de01861af0af410a
PhoenixMyron <phoenixmyron@gmail.com>
2022-12-26 11:55:54 +0600

    task 1 is done

---
+++ task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿int rec(int n, int m)
+{
+    
+    if ( m == n )
+        return n;
+    return rec(n + 1, m) + n;
+}
+
+
+
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
